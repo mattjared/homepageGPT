5457bcd50c2bb06e3e2846454d3b58150b7d5448
Matt Jared <matt.jared@vercel.com>
2023-03-14 17:01:29 -0500

    committing

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,12 +13,19 @@ const Home: NextPage = () => {
   const [generatedBios, setGeneratedBios] = useState<string>("");
   const year = new Date().getFullYear();
   const [newSiteText, setnewSiteText ] = useState("");
+  const [showFooter, setShowFooter] = useState(false);
   const cdn = "https://cdn.jsdelivr.net/npm/tailwindcss/dist/tailwind.min.css";
   const columns = 3;
-  const colorway = "red";
+  const [colorway, setColorWay] = useState("");
   const prompt = `
-    write html and website content for a business that specializes in ${bio} for ${market}. The page should be styled with tailwindcss (using this cdn ${cdn}). The page should contain a hero section with a relevant headline about ${bio}. After that there should be a section with ${columns} columns of paragraphs containing relevant content for our business that does ${bio} for ${market}. Any colors used should go with ${colorway}. The copyright year in the footer should be the ${year}.
+    write html and website content for a business that specializes in ${bio} for ${market}. The page should be styled with tailwindcss (using this cdn ${cdn}). The page should contain a hero section with a relevant headline about ${bio} to the left or right and a large call to action button opposite. After that there should be a section with ${columns} columns of paragraphs containing relevant content for our business that does ${bio} for ${market}. Finally given space and time there should be a call to action button or a contact form on the opposite side of the button from before with a relevant ${bio} picture sourced from unsplash.com opposite. Any colors used should be ${colorway} or complement it in some way. The copyright year in the footer should be the ${year}.
   `
+  const bioRef = useRef<null | HTMLDivElement>(null);
+  const scrollToBios = () => {
+    if (bioRef.current !== null) {
+      bioRef.current.scrollIntoView({ block: "end", behavior: "smooth" });
+    }
+  };
   const sanitizedData = () => ({
     __html: DOMPurify.sanitize(generatedBios)
   })
@@ -60,15 +67,9 @@ const Home: NextPage = () => {
       setGeneratedBios((prev) => prev + chunkValue);
     }
     setLoading(false);
+    setShowFooter(true);
   };
-  
-  const bioRef = useRef<null | HTMLDivElement>(null);
 
-  const scrollToBios = () => {
-    if (bioRef.current !== null) {
-      bioRef.current.scrollIntoView({ behavior: "smooth" });
-    }
-  };
 
   return (
     <div>
@@ -80,32 +81,46 @@ const Home: NextPage = () => {
       <div className="p-10 flex flex-col mx-auto max-w-3xl">
         <div className="w-full">
           <h3 className="text-3xl border-b pb-4 mb-10 font-black text-slate-900">Fill out these prompts ⤵️</h3>
-          <div className="flex mt-10 items-center space-x-3">
+          <div className="flex mt-5 items-center space-x-3">
             <p className="text-left font-medium">
               1. What does your company do?{" "}
             </p>
           </div>
-          <textarea
+          <input
             value={bio}
             onChange={(e) => setBio(e.target.value)}
-            rows={1}
-            className="w-full rounded-md border-gray-300 shadow-sm focus:border-black focus:ring-black my-5"
+            type="text"
+            className="w-full px-4 h-10 rounded border shadow-sm focus:border-black focus:ring-black my-2 bg-gray-50"
             placeholder={
               "e.g. SaaS. AI platform. Serverless database. Project management software. Troll twitter."
             }
           />
-          <div className="flex mt-10 items-center space-x-3">
+          <div className="flex mt-5 items-center space-x-3">
             <p className="text-left font-medium">
               2. What market do you serve?{" "}
             </p>
           </div>
-          <textarea
+          <input
             value={market}
             onChange={(e) => setMarket(e.target.value)}
-            rows={1}
-            className="w-full px-4 h-20 rounded border shadow-sm focus:border-black focus:ring-black my-5 bg-gray-50"
+            type="text"
+            className="w-full px-4 h-10 rounded border shadow-sm focus:border-black focus:ring-black my-2 bg-gray-50"
             placeholder={
               "e.g. Developers, Project managers, Construction workers, millenials. Lawyers"
+            }
+          />
+          <div className="flex mt-5 items-center space-x-3">
+            <p className="text-left font-medium">
+              3. What is your brand color?{" "}
+            </p>
+          </div>
+          <input
+            value={colorway}
+            onChange={(e) => setColorWay(e.target.value)}
+            type="text"
+            className="w-full px-4 h-10 rounded border shadow-sm focus:border-black focus:ring-black my-2 bg-gray-50"
+            placeholder={
+              "e.g. blue"
             }
           />
           {!loading && (
@@ -125,9 +140,8 @@ const Home: NextPage = () => {
             </button>
           )}
         </div>
-        <Footer />
       </div>
-      <h3 className="text-3xl font-black text-slate-900 text-center pb-4 mb-10 mt-20 border-b max-w-3xl mx-auto" ref={bioRef}>{newSiteText}</h3>
+      {newSiteText && <div className="text-3xl font-black text-slate-900 text-center pb-4 mb-10 mt-20 border-b max-w-3xl mx-auto" ref={bioRef}>{newSiteText}</div>}
       {generatedBios && (
         <div className="mt-10">
           <div className="flex flex-row">
@@ -150,6 +164,11 @@ const Home: NextPage = () => {
               />
             </div>   
           </div>
+        </div>
+      )}
+      {showFooter && (
+        <div className="pt-3 mt-4 flex flex-col mx-auto max-w-3xl">
+          <Footer />
         </div>
       )}
     </div>
